4fb17df23939875d652716476edee988554b0185
notalan <alan.cheng@ucdconnect.ie>
2018-02-18 15:51:03 +0000

    removed some redundant code

--- src/board/Board.java
+++ src/board/Board.java
@@ -2,14 +2,21 @@ package board;
 import property.Property;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Board extends JFrame {
     public Board(){
         JFrame panopoly = new JFrame();
         panopoly.setTitle("Panopoly");
-        panopoly.setSize(1280, 720);
-        panopoly.setLocationByPlatform(true);
         panopoly.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panel = new JPanel(new GridLayout());
+        panel.setPreferredSize(new Dimension(1280,720)); // can resize it if needed
+
+        JLabel test = new JLabel("Example of adding a label in");
+        test.setHorizontalAlignment(JLabel.CENTER);
+        test.setVerticalAlignment(JLabel.CENTER);
+        panel.add(test);
+        panopoly.add(panel);
     }
 
 }
