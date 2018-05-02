433314a648fad8d6531fff8e44554546b3693e53
Liam-Braiden <liam.braiden@ucdconnect.ie>
2018-05-02 14:38:02 +0100

    MiniGame and Shop now have initialising classes

---
+++ src/property/MiniGame.java
@@ -0,0 +1,10 @@
+package property;
+
+public class MiniGame extends Tile {
+
+    public MiniGame(String name, int xco, int yco) {
+        super.NAME = name;
+        super.XCO = xco;
+        super.YCO = yco;
+    }
+}
---
+++ src/property/Shop.java
@@ -0,0 +1,10 @@
+package property;
+
+public class Shop extends Tile {
+
+    public Shop(String name, int xco, int yco) {
+        super.NAME = name;
+        super.XCO = xco;
+        super.YCO = yco;
+    }
+}
