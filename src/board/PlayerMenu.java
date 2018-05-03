6941cc50211d2dcb108c0ce92f8fb526678533e7
notalan <alan.cheng@ucdconnect.ie>
2018-05-03 21:02:29 +0100

    Fixed player menu, can return player names, need to add ai

--- src/board/PlayerMenu.java
+++ src/board/PlayerMenu.java
@@ -8,9 +8,9 @@ import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 
 public class PlayerMenu extends JFrame{
+    private String[] player_names = new String[4];
 
-
-    public PlayerMenu(Board panopoly) throws MalformedURLException {
+    public PlayerMenu(Board panopoly) throws MalformedURLException{
         JFrame player_menu = new JFrame("PlayerMenu");
         player_menu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         PlayerMenuButtons player_buttons = new PlayerMenuButtons();
@@ -18,31 +18,38 @@ public class PlayerMenu extends JFrame{
         menu_image.setLayout(new BoxLayout(menu_image, BoxLayout.Y_AXIS));
         player_menu.add(menu_image);
 
+        menu_image.add(Box.createRigidArea(new Dimension(0, 150)));
         JLabel p1 = new JLabel("Enter Player 1 name:");
-        p1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        JLabel textbox = new JLabel();
-        textbox.setPreferredSize(new Dimension(20,20));
+        p1.setForeground(Color.white);
         menu_image.add(p1);
-        JTextField player1 = new JTextField();
-        player1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        textbox.add(player1);
-        p1.add(player1);
-        menu_image.add(textbox);
 
-//        menu_image.add(new JLabel("Enter Player 2 name:"));
-//        JTextField player2 = new JTextField();
-//        player2.setSize(100,20);
-//        menu_image.add(player2);
-//
-//        menu_image.add(new JLabel("Enter Player 3 name:"));
-//        JTextField player3 = new JTextField();
-//        player3.setSize(100,20);
-//        menu_image.add(player3);
-//
-//        menu_image.add(new JLabel("Enter Player 4 name:"));
-//        JTextField player4 = new JTextField();
-//        player4.setSize(100,20);
-//        menu_image.add(player4);
+        JTextField player1 = new JTextField();
+        player1.setHorizontalAlignment(0);
+        menu_image.add(player1);
+        menu_image.add(Box.createRigidArea(new Dimension(0, 100)));
+
+        JLabel p2 = new JLabel("Enter Player 4 name: (Leave blank for AI)");
+        p2.setForeground(Color.white);
+        menu_image.add(p2);
+        JTextField player2 = new JTextField();
+        menu_image.add(player2);
+        player2.setHorizontalAlignment(0);
+        menu_image.add(Box.createRigidArea(new Dimension(0, 100)));
+
+        JLabel p3 = new JLabel("Enter Player 4 name: (Leave blank for AI)");
+        p3.setForeground(Color.white);
+        menu_image.add(p3);
+        JTextField player3 = new JTextField();
+        player3.setHorizontalAlignment(0);
+        menu_image.add(player3);
+        menu_image.add(Box.createRigidArea(new Dimension(0, 100)));
+
+        JLabel p4 = new JLabel("Enter Player 4 name: (Leave blank for AI)");
+        p4.setForeground(Color.white);
+        menu_image.add(p4);
+        JTextField player4 = new JTextField();
+        player4.setHorizontalAlignment(0);
+        menu_image.add(player4);
 
         JButton next = player_buttons.nextButton(player_menu, panopoly);
         next.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -50,11 +57,37 @@ public class PlayerMenu extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 //set player names here
-                System.out.println(player1.getText());
+                String player_1 = player1.getText();
+                String player_2 = player2.getText();
+                String player_3 = player3.getText();
+                String player_4 = player4.getText();
+                player_names[0] = player_1;
+                if(player_2.isEmpty()){
+                    //add ai
+                }
+                else{
+                    player_names[1] = player_2;
+                }
+                if(player_3.isEmpty()){
+                    //add ai
+                }
+                else{
+                    player_names[2] = player_3;
+                }
+                if(player_4.isEmpty()){
+                    //add ai
+                }
+                else{
+                    player_names[3] = player_4;
+                }
             }
         });
+        player_menu.setUndecorated(true);
         menu_image.add(next);
         player_menu.pack();
         player_menu.setVisible(true);
+    }
+    public String[] getPlayer_names(){
+        return player_names;
     }
 }
